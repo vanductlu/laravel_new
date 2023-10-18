9b37d3477e732f85147b8e1b0248a1222d4b4f63
vanductlu <nvd2k3@gmail.com>
2023-10-18 10:28:47 +0700

    second

--- resources/views/authors/create.blade.php
+++ resources/views/authors/create.blade.php
@@ -1,34 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"> 
+@extends('layouts.app')
 
-</head>
-<body>
-@if($errors->any())
-  <div class="alert alert-danger">
-    <ul>
-      @foreach ($errors->all() as $error)
-        <li>{{ $error }}</li>
-      @endforeach
-    </ul>
-  </div>
-@endif
-<form action="{{ route('authors.store') }}" method="POST">
-<div class="form-group">
-<label for="name">Name:</label>
-<input type="text" name="name" class="form-control" required>
-</div>
-<div class="form-group">
-<label for="bio">Bio:</label>
-<textarea name="bio" class="form-control" required></textarea>
-</div>
-<div class="form-group">
-<button type="submit" class="btn btn-primary">Submit</button>
-</div>
-</form>
-</body>
-</html>
+@section('content')
+    <div class="container">
+        <h1>Tạo tác giả</h1>
+
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        <form method="POST" action="{{ route('authors.store') }}">
+            @csrf
+
+            <div class="form-group">
+                <label for="name">Tên tác giả</label>
+                <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}">
+            </div>
+
+            <button type="submit" class="btn btn-primary">Tạo</button>
+        </form>
+    </div>
+@endsection
--- resources/views/authors/index.blade.php
+++ resources/views/authors/index.blade.php
@@ -15,12 +15,13 @@
                 <tr>
                     <td>{{ $author->name }}</td>
                     <td>
-                        <a href="{{ route('authors.show', $author) }}" class="btn btn-primary">Show<i class="fa fa-eye"></i></a>
-                        <a href="{{ route('authors.edit', $author) }}" class="btn btn-warning">EDIT<i class="fa fa-edit"></i></a>
+                        <a href="{{ route('authors.show', $author) }}" class="btn btn-primary">Show <i class="fa fa-eye"></i></a>
+                        <a href="{{ route('authors.create',$author) }}" class="btn btn-success">Create <i class="fas fa-plus"></i></a>
+                        <a href="{{ route('authors.edit', $author) }}" class="btn btn-warning">EDIT <i class="fa fa-edit"></i></a>
                         <form action="{{ route('authors.destroy', $author) }}" method="POST" style="display: inline-block;">
                             @csrf
                             @method('DELETE')
-                            <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this author?')">Delete<i class="fa fa-trash"></i></button>
+                            <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this author?')">Delete <i class="fa fa-trash"></i></button>
                         </form>
                     </td>
                 </tr>
